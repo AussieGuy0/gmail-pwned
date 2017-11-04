6adfd2cc7dc088424dd519b05e70d89abc058aa6
AussieGuy0 <anthony.bruno196@gmail.com>
2017-11-04 11:17:10 +1030

    Prevent multiple icons appearing for one email, quicker gets from cache

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -5,82 +5,7 @@ const readyStateCheckInterval = setInterval(function () {
     }
 }, 10);
 
-const warnedDivs = [];
-
-function main() {
-    const jobQueue = new JobQueue(2000);
-    const emailDivs = document.querySelectorAll("*[email]");
-
-    emailDivs.forEach((emailDiv) => {
-        const email = emailDiv.getAttribute('email');
-        jobQueue.addJob(() => checkBreaches(email, emailDiv));
-
-    });
-
-    const observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-            const node = mutation.target;
-            if (node.children.length === 0) {
-                handleElement(node);
-            } else {
-                const emailElements = node.querySelectorAll('*[email]');
-                emailElements.forEach((emailElement) => handleElement(emailElement))
-            }
-
-            function handleElement(node) {
-                if (node.hasAttributes() && node.getAttribute('email') != null && warnedDivs.indexOf(node) === -1) {
-                    const email = node.getAttribute('email');
-                    jobQueue.addJob(() => checkBreaches(email, node));
-                    warnedDivs.push(node);
-                }
-
-            }
-
-        })
-    });
-    observer.observe(document.body, {childList: true, subtree: true})
-}
-
-function checkBreaches(email, emailDiv) {
-    getBreaches(email).then((json) => {
-        if (json.length > 0) {
-            const breachedElement = document.createElement('img');
-            breachedElement.classList.add('pwned-warning-icon');
-            breachedElement.setAttribute('src', chrome.runtime.getURL('images/warning.png'));
-            breachedElement.setAttribute('title', JSON.stringify(json));
-            breachedElement.textContent = json.length + ' breaches';
-            emailDiv.appendChild(breachedElement);
-        }
-    });
-}
-
-const breachUrl = 'https://haveibeenpwned.com/api/v2/breachedaccount/';
-
-function getBreaches(email) {
-    const cached = localStorage.getItem(email);
-    if (cached != null) {
-        return new Promise((resolved) => {
-            resolved(JSON.parse(cached));
-        })
-    } else {
-        return fetch(breachUrl + email)
-            .then((response) => {
-                if (response.status === 200) {
-                    return response.json();
-                } else if (response.status = 404) {
-                    return [];
-                    // return new Promise((resolved) => resolved([]));
-                }
-            }).then((json) => {
-                localStorage.setItem(email, JSON.stringify(json));
-                return json;
-            })
-    }
-}
-
-
 class JobQueue {
-
     constructor(timeBetweenJobs) {
         this.timeInterval = timeBetweenJobs;
         this.queue = [];
@@ -97,7 +22,99 @@ class JobQueue {
             throw Error('Job must be function');
         }
         this.queue.push(job);
+        return job;
+    }
+}
+
+const warnedDivs = [];
+const jobQueue = new JobQueue(2000);
+const imgClass = 'pwned-warning-icon';
+
+function main() {
+    const emailDivs = document.querySelectorAll("*[email]");
+
+    emailDivs.forEach((emailDiv) => {
+        const email = emailDiv.getAttribute('email');
+        getBreachesAndInsertIcon(email, emailDiv);
+
+    });
+
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            const node = mutation.target;
+            if (node.children.length === 0) {
+                handleElement(node);
+            } else {
+                const emailElements = node.querySelectorAll('*[email]');
+                emailElements.forEach((emailElement) => handleElement(emailElement))
+            }
+
+            function handleElement(node) {
+                if (node.hasAttributes() && node.getAttribute('email') != null && warnedDivs.indexOf(node) === -1) {
+                    const email = node.getAttribute('email');
+                    getBreachesAndInsertIcon(email, node);
+                    warnedDivs.push(node);
+                }
+
+            }
+
+        })
+    });
+    observer.observe(document.body, {childList: true, subtree: true})
+}
+
+function getBreachesAndInsertIcon(email, emailDiv) {
+    if (emailDiv.querySelectorAll(`img.${imgClass}`).length > 0) { //emailDiv already has icon
+        return;
     }
 
+    const cached = localStorage.getItem(email);
+    if (cached != null) {
+        addBreachToDiv(JSON.parse(cached));
+    } else {
+        jobQueue.addJob(() => {
+            fetchBreaches(email).then((json) => {
+                "use strict";
+                addBreachToDiv(json);
+            });
+        })
+    }
 
+    function addBreachToDiv(json) {
+        if (json.length > 0) {
+            const breachedElement = document.createElement('img');
+            breachedElement.classList.add(imgClass);
+            breachedElement.setAttribute('src', chrome.runtime.getURL('images/warning.png'));
+            breachedElement.setAttribute('title', JSON.stringify(json));
+            breachedElement.textContent = json.length + ' breaches';
+            emailDiv.appendChild(breachedElement);
+
+            const observer = new MutationObserver((e) => {
+                "use strict";
+                if (emailDiv.querySelectorAll(`img.${imgClass}`).length === 0) {
+                    getBreachesAndInsertIcon(email, emailDiv);
+                }
+            });
+            observer.observe(emailDiv, {childList: true});
+        }
+
+    }
 }
+
+const breachUrl = 'https://haveibeenpwned.com/api/v2/breachedaccount/';
+
+function fetchBreaches(email) {
+    return fetch(breachUrl + email)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else if (response.status = 404) {
+                return [];
+            }
+        }).then((json) => {
+            localStorage.setItem(email, JSON.stringify(json));
+            return json;
+        })
+}
+
+
